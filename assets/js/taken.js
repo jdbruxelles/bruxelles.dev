bd539b75cc8c311ad6fa94e72e26f82d1bf1bb8d
José dBruxelles <18559798+jdbruxelles@users.noreply.github.com>
2022-11-09 12:21:38 +0100

    Add link

--- assets/js/taken.js
+++ assets/js/taken.js
@@ -1,11 +1,13 @@
 // There is a list of taken subdomain names for bruxelles.dev domain.
 export const taken = [
+  "@",
   "app",
   "css",
   "dactylo",
   "*.dt",
   "*.jd",
   "jose",
+  "link",
   "lisibl",
   "nt-challenge",
   "si18n.js"
